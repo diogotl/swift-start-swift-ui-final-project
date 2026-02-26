76192cf98ed16941d666682f1a7663530aba47db
Diogo <diogoteixeiralucas@pm.me>
2026-02-26 20:13:11 +0000

    Initial Commit

---
+++ swift-start-swift-ui-final-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-start-swift-ui-final-project
+//
+//  Created by Diogo on 26/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-start-swift-ui-final-project/swift_start_swift_ui_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_start_swift_ui_final_projectApp.swift
+//  swift-start-swift-ui-final-project
+//
+//  Created by Diogo on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct swift_start_swift_ui_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
