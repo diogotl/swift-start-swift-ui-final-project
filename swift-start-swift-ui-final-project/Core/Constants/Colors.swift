31a62e219f3dfce82b2643308faf04804d1a2341
Diogo <diogoteixeiralucas@pm.me>
2026-03-01 18:30:57 +0000

    Rename neutral colors

--- swift-start-swift-ui-final-project/Core/Constants/Colors.swift
+++ swift-start-swift-ui-final-project/Core/Constants/Colors.swift
@@ -9,34 +9,22 @@ import SwiftUI
 
 struct Colors {
 
-    // 2C2717  -> (44, 39, 23)
     static let brown500 = Color(red: 44 / 255.0, green: 39 / 255.0, blue: 23 / 255.0)
-
-    // BEBCB7 -> (190, 188, 183)
     static let brown200 = Color(red: 190 / 255.0, green: 188 / 255.0, blue: 183 / 255.0)
-
-    // DFDFDC -> (223, 223, 220)
     static let brown100 = Color(red: 223 / 255.0, green: 223 / 255.0, blue: 220 / 255.0)
 
-    // 00A991 -> (0, 169, 145)
     static let emerald500 = Color(red: 0 / 255.0, green: 169 / 255.0, blue: 145 / 255.0)
-
-    // D9F2EF -> (217, 242, 239)
     static let emerald100 = Color(red: 217 / 255.0, green: 242 / 255.0, blue: 239 / 255.0)
 
-    // 090909 -> (9, 9, 9)
-    static let neutral900 = Color(red: 9 / 255.0, green: 9 / 255.0, blue: 9 / 255.0)
-
-    //2C2C2F
-    static let n50 = Color(red: 250 / 255.0, green: 250 / 255.0, blue: 250 / 255.0)
-    static let n100 = Color(red: 245 / 255.0, green: 245 / 255.0, blue: 245 / 255.0)
-    static let n200 = Color(red: 229 / 255.0, green: 229 / 255.0, blue: 229 / 255.0)
-    static let n300 = Color(red: 212 / 255.0, green: 212 / 255.0, blue: 212 / 255.0)
-    static let n400 = Color(red: 163 / 255.0, green: 163 / 255.0, blue: 163 / 255.0)
-    static let n500 = Color(red: 115 / 255.0, green: 115 / 255.0, blue: 115 / 255.0)
-    static let n600 = Color(red: 82 / 255.0, green: 82 / 255.0, blue: 82 / 255.0)
-    static let n700 = Color(red: 64 / 255.0, green: 64 / 255.0, blue: 64 / 255.0)
-    static let n800 = Color(red: 38 / 255.0, green: 38 / 255.0, blue: 38 / 255.0)
-    static let n900 = Color(red: 23 / 255.0, green: 23 / 255.0, blue: 23 / 255.0)
-    static let n950 = Color(red: 10 / 255.0, green: 10 / 255.0, blue: 10 / 255.0)
+    static let neutral50 = Color(red: 250 / 255.0, green: 250 / 255.0, blue: 250 / 255.0)
+    static let neutral100 = Color(red: 245 / 255.0, green: 245 / 255.0, blue: 245 / 255.0)
+    static let neutral200 = Color(red: 229 / 255.0, green: 229 / 255.0, blue: 229 / 255.0)
+    static let neutral300 = Color(red: 212 / 255.0, green: 212 / 255.0, blue: 212 / 255.0)
+    static let neutral400 = Color(red: 163 / 255.0, green: 163 / 255.0, blue: 163 / 255.0)
+    static let neutral500 = Color(red: 115 / 255.0, green: 115 / 255.0, blue: 115 / 255.0)
+    static let neutral600 = Color(red: 82 / 255.0, green: 82 / 255.0, blue: 82 / 255.0)
+    static let neutral700 = Color(red: 64 / 255.0, green: 64 / 255.0, blue: 64 / 255.0)
+    static let neutral800 = Color(red: 38 / 255.0, green: 38 / 255.0, blue: 38 / 255.0)
+    static let neutral900 = Color(red: 23 / 255.0, green: 23 / 255.0, blue: 23 / 255.0)
+    static let neutral950 = Color(red: 10 / 255.0, green: 10 / 255.0, blue: 10 / 255.0)
 }
