1256bffbf91c07af944fa383d090040dd83b45e2
Diogo <diogoteixeiralucas@pm.me>
2026-03-01 18:31:20 +0000

    Add Artwork DTOs and mapping to domain

--- swift-start-swift-ui-final-project/Features/Models/Artwork.swift
+++ swift-start-swift-ui-final-project/Features/Models/Artwork.swift
@@ -7,7 +7,61 @@
 
 import Foundation
 
-struct Artwork: Decodable, Identifiable {
+struct Artwork: Identifiable {
+    let id: Int
+    let title: String
+    let artistTitle: String?
+    let dateDisplay: String?
+    let imageID: String?
+
+    // artwork detail
+    let provenanceText: String?
+    let dimensions: String?
+    let mediumDisplay: String?
+    let placeOfOrigin: String?
+    let description: String?
+
+    // home init
+    init(id: Int, title: String, artistTitle: String?, dateDisplay: String?, imageID: String?) {
+        self.id = id
+        self.title = title
+        self.artistTitle = artistTitle
+        self.dateDisplay = dateDisplay
+        self.imageID = imageID
+        self.provenanceText = nil
+        self.dimensions = nil
+        self.mediumDisplay = nil
+        self.placeOfOrigin = nil
+        self.description = nil
+    }
+
+    // details init
+    init(
+        id: Int,
+        title: String,
+        artistTitle: String?,
+        dateDisplay: String?,
+        imageID: String?,
+        provenanceText: String?,
+        dimensions: String?,
+        mediumDisplay: String?,
+        placeOfOrigin: String?,
+        description: String?
+    ) {
+        self.id = id
+        self.title = title
+        self.artistTitle = artistTitle
+        self.dateDisplay = dateDisplay
+        self.imageID = imageID
+        self.provenanceText = provenanceText
+        self.dimensions = dimensions
+        self.mediumDisplay = mediumDisplay
+        self.placeOfOrigin = placeOfOrigin
+        self.description = description
+    }
+}
+
+struct ArtworkListDTO: Decodable {
     let id: Int
     let title: String
     let artistTitle: String?
@@ -23,6 +77,74 @@ struct Artwork: Decodable, Identifiable {
     }
 }
 
-struct ArtworkResponse: Decodable {
-    let data: [Artwork]
+struct ArtworkDetailDTO: Decodable {
+    let id: Int
+    let title: String
+    let artistTitle: String?
+    let dateDisplay: String?
+    let imageID: String?
+    let provenanceText: String?
+    let dimensions: String?
+    let mediumDisplay: String?
+    let placeOfOrigin: String?
+    let description: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case artistTitle = "artist_title"
+        case dateDisplay = "date_display"
+        case imageID = "image_id"
+        case provenanceText = "provenance_text"
+        case dimensions
+        case mediumDisplay = "medium_display"
+        case placeOfOrigin = "place_of_origin"
+        case description
+    }
+}
+
+struct ArtworkListResponse: Decodable {
+    let data: [ArtworkListDTO]
+}
+
+struct ArtworkDetailResponse: Decodable {
+    let data: ArtworkDetailDTO
+}
+
+// home DTO
+extension ArtworkListDTO {
+    func toDomain() -> Artwork {
+        return Artwork(
+            id: id,
+            title: title,
+            artistTitle: artistTitle,
+            dateDisplay: dateDisplay,
+            imageID: imageID
+        )
+    }
+}
+
+// details DTO
+extension ArtworkDetailDTO {
+    func toDomain() -> Artwork {
+        return Artwork(
+            id: id,
+            title: title,
+            artistTitle: artistTitle,
+            dateDisplay: dateDisplay,
+            imageID: imageID,
+            provenanceText: provenanceText,
+            dimensions: dimensions,
+            mediumDisplay: mediumDisplay,
+            placeOfOrigin: placeOfOrigin,
+            description: description
+        )
+    }
+}
+
+// TODO: check if it works
+extension Array where Element == ArtworkListDTO {
+    func toDomain() -> [Artwork] {
+        return self.map { $0.toDomain() }
+    }
 }
