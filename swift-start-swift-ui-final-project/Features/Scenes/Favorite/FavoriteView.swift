cfa999c550890c6d9d92df4e07267112c2c729b6
Diogo <diogoteixeiralucas@pm.me>
2026-03-03 10:24:42 +0000

    Create FavoriteView.swift

---
+++ swift-start-swift-ui-final-project/Features/Scenes/Favorite/FavoriteView.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteView.swift
+//  swift-start-swift-ui-final-project
+//
+//  Created by Diogo on 01/03/2026.
+//
+
+// TODO: favourites page
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text("favorites")
+    }
+}
