f47087ffb8752a1d115c9091771e5746af564aec
Diogo <diogoteixeiralucas@pm.me>
2026-03-03 09:06:42 +0000

    Add Artic logo and update neutral color palette

--- swift-start-swift-ui-final-project/Core/Constants/Colors.swift
+++ swift-start-swift-ui-final-project/Core/Constants/Colors.swift
@@ -16,15 +16,30 @@ struct Colors {
     static let emerald500 = Color(red: 0 / 255.0, green: 169 / 255.0, blue: 145 / 255.0)
     static let emerald100 = Color(red: 217 / 255.0, green: 242 / 255.0, blue: 239 / 255.0)
 
-    static let neutral50 = Color(red: 250 / 255.0, green: 250 / 255.0, blue: 250 / 255.0)
-    static let neutral100 = Color(red: 245 / 255.0, green: 245 / 255.0, blue: 245 / 255.0)
-    static let neutral200 = Color(red: 229 / 255.0, green: 229 / 255.0, blue: 229 / 255.0)
-    static let neutral300 = Color(red: 212 / 255.0, green: 212 / 255.0, blue: 212 / 255.0)
-    static let neutral400 = Color(red: 163 / 255.0, green: 163 / 255.0, blue: 163 / 255.0)
-    static let neutral500 = Color(red: 115 / 255.0, green: 115 / 255.0, blue: 115 / 255.0)
-    static let neutral600 = Color(red: 82 / 255.0, green: 82 / 255.0, blue: 82 / 255.0)
-    static let neutral700 = Color(red: 64 / 255.0, green: 64 / 255.0, blue: 64 / 255.0)
-    static let neutral800 = Color(red: 38 / 255.0, green: 38 / 255.0, blue: 38 / 255.0)
-    static let neutral900 = Color(red: 23 / 255.0, green: 23 / 255.0, blue: 23 / 255.0)
-    static let neutral950 = Color(red: 10 / 255.0, green: 10 / 255.0, blue: 10 / 255.0)
+//    static let neutral50 = Color(red: 250 / 255.0, green: 250 / 255.0, blue: 250 / 255.0)
+//    static let neutral100 = Color(red: 245 / 255.0, green: 245 / 255.0, blue: 245 / 255.0)
+//    static let neutral200 = Color(red: 229 / 255.0, green: 229 / 255.0, blue: 229 / 255.0)
+//    static let neutral300 = Color(red: 212 / 255.0, green: 212 / 255.0, blue: 212 / 255.0)
+//    static let neutral400 = Color(red: 163 / 255.0, green: 163 / 255.0, blue: 163 / 255.0)
+//    static let neutral500 = Color(red: 115 / 255.0, green: 115 / 255.0, blue: 115 / 255.0)
+//    static let neutral600 = Color(red: 82 / 255.0, green: 82 / 255.0, blue: 82 / 255.0)
+//    static let neutral700 = Color(red: 64 / 255.0, green: 64 / 255.0, blue: 64 / 255.0)
+//    static let neutral800 = Color(red: 38 / 255.0, green: 38 / 255.0, blue: 38 / 255.0)
+//    static let neutral900 = Color(red: 23 / 255.0, green: 23 / 255.0, blue: 23 / 255.0)
+//    static let neutral950 = Color(red: 10 / 255.0, green: 10 / 255.0, blue: 10 / 255.0)
+    
+
+
+        static let neutral50  = Color(red: 250/255.0, green: 250/255.0, blue: 250/255.0) // #FAFAFA
+        static let neutral100 = Color(red: 244/255.0, green: 244/255.0, blue: 245/255.0) // #F4F4F5
+        static let neutral200 = Color(red: 228/255.0, green: 228/255.0, blue: 231/255.0) // #E4E4E7
+        static let neutral300 = Color(red: 212/255.0, green: 212/255.0, blue: 216/255.0) // #D4D4D8
+        static let neutral400 = Color(red: 161/255.0, green: 161/255.0, blue: 170/255.0) // #A1A1AA
+        static let neutral500 = Color(red: 113/255.0, green: 113/255.0, blue: 122/255.0) // #71717A
+        static let neutral600 = Color(red:  82/255.0, green:  82/255.0, blue:  91/255.0) // #52525B
+        static let neutral700 = Color(red:  63/255.0, green:  63/255.0, blue:  70/255.0) // #3F3F46
+        static let neutral800 = Color(red:  39/255.0, green:  39/255.0, blue:  42/255.0) // #27272A
+        static let neutral900 = Color(red:  24/255.0, green:  24/255.0, blue:  27/255.0) // #18181B
+        static let neutral950 = Color(red:   9/255.0, green:   9/255.0, blue:  11/255.0) // #09090B
+
 }
